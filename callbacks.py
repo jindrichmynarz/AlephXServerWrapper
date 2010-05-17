67dab0e8916fd4d141f708913d99b2cef71562be
Jindřich Mynarz <mynarzjindrich@gmail.com>
2010-05-17 10:09:04 +0200

    Refined the Callback class

--- callbacks.py
+++ callbacks.py
@@ -11,6 +11,8 @@ class Callback(Object):
     self.results = [] # (subject, predicate, object)
     self.unmapped = [] # (XPath, value)
     self.baseName = baseName
+    self.resourceURI = "" # URI for the resource
+    self.representationURI = "" # URI for the representation
     
   def commitData(self):
     """Saves self.results into RDFLib."""
@@ -24,13 +26,24 @@ class Callback(Object):
     file.write("\n".join(map((lambda unmap: ";".join(unmap)), self.unmapped)))
     file.close()
     
-  def main(self):
+  def main(self, record):
     """Main data extraction. Must be overwritten by a child class."""
+    pass
+    
+  def addStaticTriplesGlobal(self):
+    """Method for adding triples that are stable for each database."""
+    
+    pass
+    
+  def addStaticTriplesBase(self):
+    """Method for adding triples that are stable for the specific database. Must be overwritten by a child class."""
     pass
     
   def run(self):
     """Runs the callback."""
     self.main()
+    self.addStaticTriplesGlobal()
+    self.addStaticTriplesBase()
     self.commitData()
     self.writeUnmapped()
   
@@ -40,7 +53,14 @@ class STK02Callback(Callback):
   def __init__(self, baseName="STK02"):
     Callback.__init__(self)
     
-  def main(self):
+  def main(self, record):
+    subject = record.getXPath("//fixfield[@id='001']")[0].content
+    subject = re.search("\d+$", subject).group(0).lstrip("0")
+    subject = "http://data.techlib.cz/resource/issn/%s" % (subject)
+    self.resourceURI = subject
+    self.representationURI = subject + ".rdf"
+    
+  def addStaticTriplesBase(self):
     pass
     
     
@@ -49,7 +69,10 @@ class STK10Callback(Callback):
   def __init__(self, baseName="STK10"):
     Callback.__init__(self)
     
-  def main(self):
+  def main(self, record):
+    pass
+    
+  def addStaticTriplesBase(self):
     pass
 
 
@@ -58,5 +81,8 @@ class STK01Callback(Callback):
   def __init__(self, baseName="STK01"):
     Callback.__init__(self)
     
-  def main(self):
-    pass    
+  def main(self, record):
+    pass
+    
+  def addStaticTriplesBase(self):
+    pass
