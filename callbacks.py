791b5c609a6eb69038347cf68b9da702a0c0d5a9
Jindřich Mynarz <mynarzjindrich@gmail.com>
2010-05-17 12:00:33 +0200

    Updated callbacks

--- callbacks.py
+++ callbacks.py
@@ -29,11 +29,20 @@ class Callback(Object):
   def main(self, record):
     """Main data extraction. Must be overwritten by a child class."""
     pass
+  
+  def addTriples(self, triples):
+    """Generic method for adding triples."""
+    [self.results.append(triple) for triple in triples]
     
   def addStaticTriplesGlobal(self):
     """Method for adding triples that are stable for each database."""
-    
-    pass
+    triples = [
+      (self.representationURI, "dcterms:rigthsHolder", "http://www.techlib.cz"),
+      (self.representationURI, "cc:attributionName", "Národní technická knihovna"),
+      (self.representationURI, "cc:attributionURL", self.representationURI),
+      (self.representationURI, "cc:license", "http://creativecommons.org/licenses/by-nc-sa/3.0/cz/"),
+    ]
+    self.addTriples(triples)
     
   def addStaticTriplesBase(self):
     """Method for adding triples that are stable for the specific database. Must be overwritten by a child class."""
@@ -54,14 +63,46 @@ class STK02Callback(Callback):
     Callback.__init__(self)
     
   def main(self, record):
-    subject = record.getXPath("//fixfield[@id='001']")[0].content
-    subject = re.search("\d+$", subject).group(0).lstrip("0")
+    sysno = record.getXPath("//fixfield[@id='001']")[0]
+    subject = re.search("\d+$", sysno).group(0).lstrip("0")
     subject = "http://data.techlib.cz/resource/issn/%s" % (subject)
     self.resourceURI = subject
     self.representationURI = subject + ".rdf"
+    self.results.append((self.representationURI, "dc:identifier", sysno))
     
+    # Last modified date
+    lastModified = record.getXPath("//fixfield[@id='005']")[0]
+    parseDate = re.search("^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})", lastModified)
+    lastModified = "%s-%s-%s+%s:%s" % (parseDate.group(1), parseDate.group(2), parseDate.group(3), parseDate.group(4), parseDate.group(5))
+    self.results.append((self.representationURI, "dcterms:modified", lastModified)) # Jak přidat anotaci XSD type?
+    
+    # Short title
+    shortTitle = record.getXPath('//varfield[@id="210"][@i1="1"]/subfield[@label="a"]')
+    if not shortTitle == []:
+      shortTitle = shortTitle[0]
+      self.results.append((self.resourceURI, "bibo:shortTitle", shortTitle))
+    
+    # Publisher      
+    publisher = record.getXPath('//varfield[@id="260"]/subfield[@label="b"]')
+    if not publisher == []:
+      publisher = publisher[0]
+      self.results.append((self.resourceURI, "dc:publisher", publisher))
+      # Namapovat vydavatele? PublisherMapper
+
+    # Place of publication
+    publicationPlace = record.getXPath('//varfield[@id="260"]/subfield[@label="a"]')
+    if not publicationPlace == []:
+      publicationPlace = publicationPlace[0]
+      # Dodělat:
+      #   - predikát?
+      #   - zdali to nevypovídá spíše o vydavateli?
+      # self.results.append((self.resourceURI, "", publicationPlace))
+
   def addStaticTriplesBase(self):
-    pass
+    triples = [
+      (self.resourceURI, "rdf:type", "bibo:Periodical"),
+    ]
+    self.addTriples(triples)
     
     
 class STK10Callback(Callback):
